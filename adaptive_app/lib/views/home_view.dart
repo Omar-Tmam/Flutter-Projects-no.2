94d16eecadd6eb5960af1bf8cab3ab2e19cd5d33
omar <omartmam9@gmail.com>
2025-07-27 21:07:15 +0300

    add drawer

--- adaptive_app/lib/views/home_view.dart
+++ adaptive_app/lib/views/home_view.dart
@@ -1,18 +1,32 @@
+import 'package:adaptive_app/widgets/custom_drawer.dart';
 import 'package:adaptive_app/widgets/home_view_body.dart';
 import 'package:flutter/material.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
   @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();// Scaffold key to control the drawer
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey, // Assign the scaffold key to the Scaffold widget
+      drawer: CustomDrawer(),
       backgroundColor: Color(0xffdbdbdb),
       appBar: AppBar(
         backgroundColor: Colors.black,
-        leading: Icon(
-          Icons.menu,
-          color: Colors.white,
+        leading: GestureDetector(
+          onTap: () {
+            scaffoldKey.currentState?.openDrawer(); // Open the drawer when the menu icon is tapped
+          },
+          child: Icon(
+            Icons.menu,
+            color: Colors.white,
+          ),
         ),
       ),
       body: HomeViewBody(),
