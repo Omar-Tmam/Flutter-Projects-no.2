8e4f7859b03742ac4ed3ef59e18854edce1f2ba7
omar <omartmam9@gmail.com>
2025-07-27 20:44:59 +0300

    create custom item

--- adaptive_app/lib/views/home_view.dart
+++ adaptive_app/lib/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:adaptive_app/widgets/home_view_body.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -6,6 +7,7 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xffdbdbdb),
       appBar: AppBar(
         backgroundColor: Colors.black,
         leading: Icon(
@@ -13,6 +15,7 @@ class HomeView extends StatelessWidget {
           color: Colors.white,
         ),
       ),
+      body: HomeViewBody(),
     );
   }
 }
---
+++ adaptive_app/lib/widgets/custom_sliver_grid.dart
@@ -0,0 +1,23 @@
+import 'package:adaptive_app/widgets/custom_item.dart';
+import 'package:flutter/material.dart';
+
+class CustomSliverGrid extends StatelessWidget {
+  const CustomSliverGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverGrid.builder(
+      itemCount: 4,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 16,
+        mainAxisSpacing: 16,
+      ),
+      itemBuilder: (context, index) {
+        return CustomItem();
+      },
+    );
+  }
+}
+
+
