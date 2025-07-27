be48cd389208f7d791aa3d3ed3983d55162abe28
omar <omartmam9@gmail.com>
2025-07-27 21:12:37 +0300

    add drawer widget

--- adaptive_app/lib/widgets/custom_drawer.dart
+++ adaptive_app/lib/widgets/custom_drawer.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Drawer(
+      backgroundColor: Color(0xffdbdbdb),
+      child: Column(
+        children: [
+          DrawerHeader(
+            child: Icon(
+              FontAwesomeIcons.solidHeart,
+              size: 50,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
