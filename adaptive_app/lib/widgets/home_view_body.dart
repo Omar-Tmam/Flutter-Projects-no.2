7264ca1429fa6757ebca898ac2e4a3f118886626
omar <omartmam9@gmail.com>
2025-07-28 22:14:37 +0300

    adaptive layout widget

--- adaptive_app/lib/widgets/home_view_body.dart
+++ adaptive_app/lib/widgets/home_view_body.dart
@@ -1,4 +1,4 @@
-
+import 'package:adaptive_app/widgets/adaptive_layout.dart';
 import 'package:adaptive_app/widgets/desktop_layout.dart';
 import 'package:adaptive_app/widgets/mobile_layout.dart';
 import 'package:adaptive_app/widgets/tablet_layout.dart';
@@ -11,17 +11,10 @@ class HomeViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: LayoutBuilder(
-        builder: (context, constraints) {
-          if (constraints.maxWidth < 600) {
-            return MobileLayout();
-          } else if (constraints.maxWidth < 900) {
-            return TabletLayout();
-          } else {
-            return DesktopLayout();
-          }
-        },
-      ),
+      child: AdaptiveLayout(
+          mobileLayout: MobileLayout(),
+          tabletLayout: TabletLayout(),
+          desktopLayout: DesktopLayout()),
     );
   }
 }
